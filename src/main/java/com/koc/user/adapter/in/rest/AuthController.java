43138afc988884f25142492d1c9da78ada8273c1
hgs6424 <hgs6424@naver.com>
2023-12-17 12:55:01 +0900

    clean up code

--- src/main/java/com/koc/user/adapter/in/rest/AuthController.java
+++ src/main/java/com/koc/user/adapter/in/rest/AuthController.java
@@ -8,7 +8,6 @@ import com.koc.user.domain.token.TokenDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
